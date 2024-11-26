f7def60caea318b74af855bccc4ed45c5cb4b360
nimofarah <faranim01@gmail.com>
2024-11-26 22:43:50 +0200

    Tehty

--- src/main/java/Bridge/Demo.java
+++ src/main/java/Bridge/Demo.java
@@ -1,15 +1,21 @@
 package Bridge;
 
 import Bridge.Device;
+
 import Bridge.Tv;
 import Bridge.Radio;
+import Bridge.SmartTv;
 import Bridge.BasicRemote;
 import Bridge.AdvancedRemote;
+import Bridge.SmartRemote;
 
 public class Demo {
     public static void main(String[] args) {
         testDevice(new Tv());
+
         testDevice(new Radio());
+
+        testDeviceWithSmartRemote(new SmartTv());
     }
 
     public static void testDevice(Device device) {
@@ -23,5 +29,15 @@ public class Demo {
         advancedRemote.power();
         advancedRemote.mute();
         device.printStatus();
+    }
+
+    public static void testDeviceWithSmartRemote(SmartTv smartTv) {
+        System.out.println("Tests with smart remote (voice control).");
+        SmartRemote smartRemote = new SmartRemote(smartTv);
+
+        smartRemote.power();
+        smartRemote.voiceControl("browse");
+
+        smartTv.printStatus();
     }
 }
